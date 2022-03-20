5d3c5ac57b7bbce035653b05ae23deb83db5573e
Provashish Roy <provashishroy95@gmail.com>
2022-03-20 13:24:20 +0600

    added class component

--- src/components/DeviceDetail/DeviceDetail.js
+++ src/components/DeviceDetail/DeviceDetail.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Tablet from '../Tablet/Tablet';
 
 const DeviceDetail = (props) => {
     return (
         <div>
             <p>Price : {props.price}</p>
+            <Tablet></Tablet>
         </div>
     );
 };
---
+++ src/components/Tablet/Tablet.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Tablet extends Component {
+    render() {
+        return (
+            <div>
+                <h2>this is class Component</h2>
+            </div>
+        );
+    }
+}
+
+export default Tablet;
