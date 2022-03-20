c503fbd67a38f975aa9d8baf879bd580a1242d00
Provashish Roy <provashishroy95@gmail.com>
2022-03-20 16:43:55 +0600

    added some code

--- src/components/Tablet/Tablet.js
+++ src/components/Tablet/Tablet.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
+
+
+
+const numbers = [56, 61, 15, 35];
 class Tablet extends Component {
     render() {
         return (
             <div>
                 <h2>this is class Component</h2>
+                <ul> {numbers.map(num => <li>{num}</li>)} </ul>
             </div>
         );
     }
